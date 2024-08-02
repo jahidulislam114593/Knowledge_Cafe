33ed71eb7c34004a7730523874a1360625b45056
Jahidul Islam <jahidul.islam114593@gmail.com>
2024-08-02 20:13:03 +0600

    set layout and make react responsive

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import Header from "./Components/Header/Header";
+import Blogs from "./Components/Blogs/Blogs";
+import Bookmarks from "./Components/Bookmarks/Bookmarks";
+function App() {
+  return (
+    <div>
+      <Header />
+      <div className="md:flex">
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import Profile from "../../assets/images/profile.png";
+const Header = () => {
+  return (
+    <header className="flex justify-between items-center p-4 mx-4 border-b-2">
+      <h1 className="text-4xl">Knowledge Cafe</h1>
+      <img src={Profile}></img>
+    </header>
+  );
+};
+
+export default Header;
