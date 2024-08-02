660d615385edc32cdcf0eb511aee83443209576d
Jahidul Islam <jahidul.islam114593@gmail.com>
2024-08-02 22:54:00 +0600

    display blog data and hashtag

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,6 +1,39 @@
 import PropTypes from "prop-types";
 const Blog = ({ blog }) => {
-  return <div></div>;
+  const {
+    title,
+    cover,
+    author_img,
+    author,
+    reading_time,
+    posted_date,
+    hashtag,
+  } = blog;
+  return (
+    <div>
+      <img src={cover} alt="" />
+      <div className="flex justify-between">
+        <div className="flex">
+          <img className="w-14" src={author_img} alt="" />
+          <div className="ml-6">
+            <h3 className="text-3xl">{author}</h3>
+            <p>{posted_date}</p>
+          </div>
+        </div>
+        <div>
+          <span>{reading_time} min read</span>
+        </div>
+      </div>
+      <h4 className="text-4xl">{title}</h4>
+      <p>
+        {hashtag.map((hash, idx) => (
+          <span key={idx}>
+            <a href=""> {hash}</a>
+          </span>
+        ))}
+      </p>
+    </div>
+  );
 };
 
 Blog.propTypes = {
